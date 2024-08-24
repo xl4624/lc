0b55aac748f0b2ffa7b3611d6ebf2fa124ccb8f2
rakib-shahid <rakibshahid1298@gmail.com>
2024-08-24 05:00:17 -0400

    initial

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+import config
+import os
+import asyncio
+from discord.ext import commands
+
+########################################
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="lcl$", intents=intents)
+########################################
+
+
+# On Ready
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+@bot.event
+async def load():
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            await bot.load_extension(f"cogs.{file[:-3]}")
+
+
+async def main():
+    await load()
+
+    ########################################
+    # START BOT
+    await bot.start(config.TOKEN)
+    ########################################
+
+
+asyncio.run(main())
